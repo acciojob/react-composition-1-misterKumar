7f93a518273ed37798ea5e2e5873180688d7a37f
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-12 20:40:00 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
 
-const App = () => {
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul className="tabs">
+        {tabs.map((tab, index) => (
+          <li
+            key={index}
+            className={index === activeTab ? 'active' : ''}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <div className="tab-content">
+        {tabs[activeTab].content}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const tabsData = [
+    { title: 'Tab 1', content: 'This is the content for Tab 1.'  },
+    { title: 'Tab 2', content: 'This is the content for Tab 2.'  },
+    { title: 'Tab 3', content: 'This is the content for Tab 3.' },
+  ];
+
+  return (
+    <div>
+    
+      <Tabs tabs={tabsData} />
+    </div>
+  );
+};
+
+export default App;
